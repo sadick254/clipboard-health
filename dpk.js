25d80901ee94160a3f8ebfe7d847233fe5a9b17e
sadick254 <sadickjunior@gmail.com>
2023-03-08 09:06:38 +0300

    Move definitions close to where they are used.

--- dpk.js
+++ dpk.js
@@ -2,12 +2,12 @@ const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
   if(!event) {
     return TRIVIAL_PARTITION_KEY;
   }
+  
+  let candidate;
 
   if(event.partitionKey) {
       candidate = event.partitionKey;
@@ -20,6 +20,7 @@ exports.deterministicPartitionKey = (event) => {
       candidate = JSON.stringify(candidate);
   }
   
+  const MAX_PARTITION_KEY_LENGTH = 256;
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
