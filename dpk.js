8bb9e7b8513e639ce6e26bd39b13296d2ed4dc30
sadick254 <sadickjunior@gmail.com>
2023-03-08 08:41:45 +0300

    chore: Fix partitionKey access.

--- dpk.js
+++ dpk.js
@@ -9,7 +9,7 @@ exports.deterministicPartitionKey = (event) => {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  if(event?.partitionKey) {
+  if(event.partitionKey) {
       candidate = event.partitionKey;
   } else {
       const data = JSON.stringify(event);
@@ -19,7 +19,7 @@ exports.deterministicPartitionKey = (event) => {
   if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
   }
-
+  
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
