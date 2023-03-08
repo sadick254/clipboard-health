6aec8d1cdc3f087a78e5d5fa9c64ef85c8bc519f
sadick254 <sadickjunior@gmail.com>
2023-03-08 08:20:59 +0300

    refactor: Simplify nested ifs.

--- dpk.js
+++ dpk.js
@@ -9,19 +9,15 @@ exports.deterministicPartitionKey = (event) => {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  if (event) {
-    if (event.partitionKey) {
+  if(event?.partitionKey) {
       candidate = event.partitionKey;
-    } else {
+  } else {
       const data = JSON.stringify(event);
       candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
+  if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
-    }
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
