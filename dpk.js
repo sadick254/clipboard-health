002d0ec49165f4688026a0a5e8b8107f2b79f0d7
sadick254 <sadickjunior@gmail.com>
2023-03-08 08:10:13 +0300

    refactor: Return early for empty events.

--- dpk.js
+++ dpk.js
@@ -5,6 +5,10 @@ exports.deterministicPartitionKey = (event) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate;
 
+  if(!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
   if (event) {
     if (event.partitionKey) {
       candidate = event.partitionKey;
@@ -18,8 +22,6 @@ exports.deterministicPartitionKey = (event) => {
     if (typeof candidate !== "string") {
       candidate = JSON.stringify(candidate);
     }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
